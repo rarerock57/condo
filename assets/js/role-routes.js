33d25489702087e330e07df3fb431be484c18bcb
rarerock57 <lawniyomyinde.c@gmail.com>
2026-04-20 18:32:21 +0700

    Create role-routes.js

---
+++ assets/js/role-routes.js
@@ -0,0 +1,9 @@
+window.ROLE_HOME = {
+  super_admin: '../admin/dashboard.html',
+  admin: '../admin/dashboard.html',
+  account: '../account/dashboard.html',
+  resident: '../resident/dashboard.html',
+  technician: '../technician/dashboard.html'
+};
+
+window.DEFAULT_HOME = '../auth/login.html';
